574e7a30bbe20912531f61a37f897bd67d150d65
Ezrahel <86208443+Ezrahel@users.noreply.github.com>
2022-07-13 15:42:37 +0100

    Initial commit

---
+++ Css/Js/home.js
@@ -0,0 +1,11 @@
+// function text(){
+//     let text_element = document.querySelector(".text");
+//     let text_array = text_element.innerHTML.split("");
+//     let all_words = text_element.innerHTML.split("");
+//     let text_len = text_array.length
+
+//     const word_len = all_words.map((word) =>{
+//         return word.length;
+//     });
+// }
+// text();
